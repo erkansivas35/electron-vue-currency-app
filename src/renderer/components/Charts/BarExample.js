06d1582fc7bcf02d5045545c8552c88afe3435c4
erkansivas35 <erkan.sivas35@gmail.com>
2018-09-04 11:46:05 +0300

    chart update

--- src/renderer/components/Charts/BarExample.js
+++ src/renderer/components/Charts/BarExample.js
@@ -1,17 +1,21 @@
 import { Bar } from './BaseCharts'
 
 export default {
+  props: {
+    labels: {
+      type: Array,
+      require: true
+    },
+    datasets: {
+      type: Array,
+      require: true
+    }
+  },
   extends: Bar,
-  mounted () {
+  mounted() {
     this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+      labels: this.labels,
+      datasets: this.datasets
+    }, { responsive: true, maintainAspectRatio: false })
   }
 }
--- src/renderer/components/GraphChart.vue
+++ src/renderer/components/GraphChart.vue
@@ -1,80 +1,45 @@
 <template>
-  <line-example/>
+  <bar-example :labels="labels" :datasets="datasets" />
 </template>
 
 <script>
 import BarExample from "./Charts/BarExample.js";
-import LineExample from "./Charts/LineExample";
 
 export default {
   components: {
-    BarExample,
-    LineExample
+    BarExample
   },
   data() {
     return {
-      dataPoints: null,
-      height: 20
+      labels: [
+        "04/09/2018",
+        "05/09/2018",
+        "06/09/2018",
+        "07/09/2018",
+        "08/09/2018",
+        "09/09/2018",
+        "10/09/2018",
+        "04/09/2018",
+        "05/09/2018"
+      ],
+      datasets: [
+        {
+          label: 'USD',
+          backgroundColor: "#4482fa",
+          data: [
+            60,
+            50,
+            50,
+            20,
+            60,
+            140,
+            130,
+            135,
+            100
+          ]
+        }
+      ]
     };
-  },
-  mounted() {
-    setInterval(() => {
-      this.fillData();
-    }, 2000);
-  },
-  methods: {
-    increaseHeight() {
-      this.height += 10;
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-    },
-    fillData() {
-      this.dataPoints = {
-        labels: [
-          "January" + this.getRandomInt(),
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
-        ],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt(),
-              this.getRandomInt()
-            ]
-          }
-        ]
-      };
-    }
-  },
-  computed: {
-    myStyles() {
-      return {
-        height: `${this.height}px`,
-        position: "relative"
-      };
-    }
   }
 };
 </script>
